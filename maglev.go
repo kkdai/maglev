5f1c39efc4e55f76d274c003ab22e53b29295e3c
Evan Lin <evanslin@gmail.com>
2016-06-02 16:05:31 +0800

    Add testing case and get

--- maglev.go
+++ maglev.go
@@ -1,10 +1,6 @@
 package maglev
 
-import (
-	"fmt"
-
-	"github.com/dchest/siphash"
-)
+import "github.com/dchest/siphash"
 
 const (
 	bigM uint64 = 65537
@@ -15,22 +11,74 @@ type Maglev struct {
 	n           uint64 //size of VIP backeds
 	m           uint64 //sie of the lookup table
 	permutation [][]uint64
+	lookup      []int64
+	nodeList    []string
 }
 
 //NewMaglev :
-func NewMaglev(backeds []string, m uint64) *Maglev {
-	mag := new(Maglev)
-	mag.n = uint64(len(backeds))
-	mag.m = m
+func NewMaglev(backends []string, m uint64) *Maglev {
+	mag := &Maglev{n: uint64(len(backends)), m: m}
+	mag.generatePopulation(backends)
+	mag.lookup = mag.populate()
+	mag.nodeList = backends
 	return mag
+}
+
+//Get :Get node name
+func (m *Maglev) Get(obj string) string {
+	key := m.hashKey(obj)
+	return m.nodeList[m.lookup[key%m.m]]
+}
+
+func (m *Maglev) hashKey(obj string) uint64 {
+	return siphash.Hash(0xdeadbabe, 0, []byte(obj))
 }
 
 func (m *Maglev) generatePopulation(backeds []string) {
 	for i := 0; i < len(backeds); i++ {
 		bData := []byte(backeds[i])
-		offset := siphash.Hash(0xdeadbabe, 0, bData) % bigM
-		skip := (siphash.Hash(0xdeadbeef, 0, bData) % (bigM - 1)) + 1
 
-		fmt.Println(offset, skip)
+		offset := siphash.Hash(0xdeadbabe, 0, bData) % m.m
+		skip := (siphash.Hash(0xdeadbeef, 0, bData) % (m.m - 1)) + 1
+
+		iRow := make([]uint64, m.m)
+		var j uint64
+		for j = 0; j < m.m; j++ {
+			iRow[j] = (offset + uint64(j)*skip) % m.m
+		}
+
+		m.permutation = append(m.permutation, iRow)
 	}
+}
+
+//Populate :
+func (m *Maglev) populate() []int64 {
+	var i, j uint64
+	next := make([]uint64, m.n)
+	entry := make([]int64, m.m)
+	for j = 0; j < m.m; j++ {
+		entry[j] = -1
+	}
+
+	var n uint64
+
+	for { //true
+		for i = 0; i < m.n; i++ {
+			c := m.permutation[i][next[i]]
+			for entry[c] >= 0 {
+				next[i] = next[i] + 1
+				c = m.permutation[i][next[i]]
+			}
+
+			entry[c] = int64(i)
+			next[i] = next[i] + 1
+			n++
+
+			if n == m.m {
+				return entry
+			}
+		}
+
+	}
+
 }
---
+++ maglev_test.go
@@ -0,0 +1,51 @@
+package maglev
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+// func TestPopulate(t *testing.T) {
+
+// 	var tests = []struct {
+// 		dead []int
+// 		want []int
+// 	}{
+// 		{nil, []int{1, 0, 1, 0, 2, 2, 0}},
+// 		{[]int{1}, []int{0, 0, 0, 0, 2, 2, 2}},
+// 	}
+
+// 	permutations := [][]uint64{
+// 		{3, 0, 4, 1, 5, 2, 6},
+// 		{0, 2, 4, 6, 1, 3, 5},
+// 		{3, 4, 5, 6, 0, 1, 2},
+// 	}
+
+// 	for _, tt := range tests {
+// 		if got := populate(permutations, tt.dead); !reflect.DeepEqual(got, tt.want) {
+// 			t.Errorf("populate(...,%v)=%v, want %v", tt.dead, got, tt.want)
+// 		}
+// 	}
+// }
+
+const sizeN = 5
+const lookupSizeM = 13 //need prime and
+
+func TestDistribution(t *testing.T) {
+	// const size = 10
+
+	var names []string
+	for i := 0; i < sizeN; i++ {
+		names = append(names, fmt.Sprintf("backend-%d", i))
+	}
+
+	mm := NewMaglev(names, lookupSizeM)
+	log.Println("kk1:", mm.permutation[0])
+	log.Println("pp1:", mm.populate())
+	log.Println("pp2:", mm.populate())
+	log.Println("node1:", mm.Get("IP1"))
+	log.Println("node2:", mm.Get("IP2"))
+	log.Println("node3:", mm.Get("IPasdasdwni2"))
+
+}
