49dbb67f5dc94e191361aba55e27e19843fd31f0
Vincent Raman <vincent.raman@macq.eu>
2019-05-05 15:30:25 +0200

    Reset permutation table before regenerating ones, avoids memory leak

--- maglev_test.go
+++ maglev_test.go
@@ -3,6 +3,7 @@ package maglev
 import (
 	"fmt"
 	"log"
+	"sort"
 	"testing"
 )
 
@@ -37,26 +38,43 @@ func TestDistribution(t *testing.T) {
 	for i := 0; i < sizeN; i++ {
 		names = append(names, fmt.Sprintf("backend-%d", i))
 	}
-
-	mm := NewMaglev(names, lookupSizeM)
+	mm, err := NewMaglev(names, lookupSizeM)
+	if err != nil {
+		t.Error("Creation failed", err)
+	}
 	v, err := mm.Get("IP1")
+	if err != nil {
+		t.Error("Get failed", err)
+	}
 	log.Println("node1:", v)
-	v, _ = mm.Get("IP2")
+	v, err = mm.Get("IP2")
+	if err != nil {
+		t.Error("Get failed", err)
+	}
 	log.Println("node2:", v)
-	v, _ = mm.Get("IPasdasdwni2")
+	v, err = mm.Get("IPasdasdwni2")
+	if err != nil {
+		t.Error("Get failed", err)
+	}
 	log.Println("node3:", v)
 	log.Println("lookup:", mm.lookup)
 	if err := mm.Remove("backend-0"); err != nil {
 		t.Error("Remove failed", err)
 	}
 	log.Println("lookup:", mm.lookup)
-	v, _ = mm.Get("IPasdasdwni2")
+	v, err = mm.Get("IPasdasdwni2")
+	if err != nil {
+		t.Error("Get failed", err)
+	}
 	log.Println("node3-D:", v)
 
 	if err := mm.Remove("backend-1"); err != nil {
 		t.Error("Remove failed", err)
 	}
-	v, _ = mm.Get("IP2")
+	v, err = mm.Get("IP2")
+	if err != nil {
+		t.Error("Get failed", err)
+	}
 	log.Println("node2-D:", v)
 
 	mm.Remove("backend-2")
@@ -65,5 +83,122 @@ func TestDistribution(t *testing.T) {
 
 	if _, err = mm.Get("IP1"); err == nil {
 		t.Error("Empty handle error")
+	}
+}
+
+func TestSetAddRemove(t *testing.T) {
+	var names []string
+	for i := 0; i < sizeN; i++ {
+		names = append(names, fmt.Sprintf("backend-%d", i))
+	}
+
+	mm, err := NewMaglev(names, lookupSizeM)
+	if err != nil {
+		t.Error("Creation failed", err)
+	}
+
+	err = mm.Add("backend-test")
+	if err != nil {
+		t.Error("Add failed", err)
+	}
+	if (uint64)(len(mm.lookup)) != mm.m {
+		t.Error("lookup size not correct")
+	}
+	if len(mm.permutation) != len(mm.nodeList) {
+		t.Error("permutation size not correct")
+	}
+	if len(names) != sizeN {
+		t.Error("Original values has been modified")
+	}
+
+	err = mm.Remove("backend-test")
+	if err != nil {
+		t.Error("Remove failed", err)
+	}
+	err = mm.Remove(names[0])
+	if err != nil {
+		t.Error("Remove failed", err)
+	}
+	if (uint64)(len(mm.lookup)) != mm.m {
+		t.Error("lookup size not correct")
+	}
+	if len(mm.permutation) != len(mm.nodeList) {
+		t.Error("permutation size not correct")
+	}
+	if len(names) != sizeN {
+		t.Error("Original values has been modified")
+	}
+
+	err = mm.Set([]string{"backend-0", "backend-1"})
+	if err != nil {
+		t.Error("Remove failed", err)
+	}
+	if (uint64)(len(mm.lookup)) != mm.m {
+		t.Error("lookup size not correct")
+	}
+	if len(mm.permutation) != len(mm.nodeList) {
+		t.Error("permutation size not correct")
+	}
+
+	for i := 0; i < lookupSizeM+1; i++ {
+		names = append(names, fmt.Sprintf("backend-%d", i))
+	}
+	err = mm.Set(names)
+	if err == nil {
+		t.Error("No error thrown when trying to set too many backends")
+	}
+}
+
+func TestRemovedBackend(t *testing.T) {
+	var names []string
+	for i := 0; i < sizeN; i++ {
+		names = append(names, fmt.Sprintf("backend-%d", i))
+	}
+	mm, err := NewMaglev(names, lookupSizeM)
+	if err != nil {
+		t.Error("Creation failed", err)
+	}
+
+	var lookUpNames []string
+	for i := 0; i < 1024; i++ {
+		lookUpNames = append(lookUpNames, fmt.Sprintf("IP%d", i))
+	}
+
+	var backendFound []string
+	for _, lookupName := range lookUpNames {
+		name, err := mm.Get(lookupName)
+		if err != nil {
+			t.Error("Get failed", err)
+		}
+		if sort.SearchStrings(backendFound, name) == len(backendFound) {
+			backendFound = append(backendFound, name)
+		}
+	}
+
+	if len(backendFound) < 2 {
+		t.Error("Distribution failed")
+	}
+
+	for i, name := range names {
+		if i != 3 { // Remove a node in the middle
+			if err := mm.Remove(name); err != nil {
+				t.Error("Remove failed", err)
+			}
+		}
+	}
+
+	backendFound = nil
+	for _, lookupName := range lookUpNames {
+		name, err := mm.Get(lookupName)
+		if err != nil {
+			t.Error("Get failed", err)
+		}
+		if sort.SearchStrings(backendFound, name) == len(backendFound) {
+			backendFound = append(backendFound, name)
+		}
+	}
+
+	if len(backendFound) != 1 {
+		t.Error("Distribution failed")
 	}
 }
